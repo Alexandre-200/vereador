96a2558fb2826c5073a91da3fa14df1075ae92f3
Alexandre-200 <alexandre_200@outlook.com.br>
2024-07-15 20:56:44 -0300

    atualizacao Vlibras

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "./globals.css";
-import VLibras from 'vlibras-nextjs';
 
 export const metadata = {
   title: "Almeida 12345",
